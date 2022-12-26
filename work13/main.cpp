364d4bb921e968733fa28daa8475263f4b0960ba
Denys Balan <89107241+hogg21@users.noreply.github.com>
2022-12-26 11:17:23 +0200

    Add files via upload

---
+++ work13/main.cpp
@@ -0,0 +1,21 @@
+#include <windows.h>
+
+/* This is where all the input to the window goes to */
+class Sin {
+	private:
+		int a, b
+	public:
+		sin() {
+			return b / a;
+		}
+};
+
+class Cos {
+	Sin s;
+	private:
+		int a;
+	protected:
+		cos() {
+			return a / s.sin();
+		}
+};
